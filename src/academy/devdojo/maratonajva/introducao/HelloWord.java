5da9316291591db08f1a7783b2d44faaccb81350
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-11-20 13:53:58 -0300

    up

---
+++ src/academy/devdojo/maratonajva/introducao/HelloWord.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajva.introducao;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/academy/devdojo/maratonajva/introducao/Tiposprimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajva.introducao;
+
+public class Tiposprimitivos {
+    public static void main(String[] args) {
+        int numero = (int) 100000000000000L; //casting
+        long numerog;
+        boolean numerob;
+        char letra='e';
+        double b= 111;
+        byte a = 10;
+        short ba = 1;
+        float z= 1.3f;
+        System.out.println(numero);
+    }
+
+}
